1a7ea921a59623bca24747aed53ea1c0f4b08183
Chad Heuschober <chad.heuschober@cuny.edu>
2014-09-20 19:00:22 -0400

    Offers more explicit variable naming.

--- task_02.py
+++ task_02.py
@@ -8,59 +8,59 @@ import decimal
 # Your functions should not take string representations of these values
 # Delete these lines when function conversion is complete
 PSTR = raw_input('What is the amount of your principal?: ')
-TSTR = raw_input('For how many years is this loan being borrowed?: ')
+DSTR = raw_input('For how many years is this loan being borrowed?: ')
 QSTR = raw_input('Are you prequalified for this loan?: ')
 
 # Function arguments should not need the following three transformations
 # Delete these lines when function conversion is complete
-P = int(PSTR)
-T = int(TSTR)
-Q = True if QSTR.lower()[0] == 'y' else False
+PRINCIPAL = int(PSTR)
+DURATION = int(DSTR)
+PREQUALIFICATION = True if QSTR.lower()[0] == 'y' else False
 
 # Function conversion work you do should start here
-N = 12
-R = None
+INTERVAL = 12
+RATE = None
 TOTAL = None
 
-if P >= 0 and P <= 199999:
-    if T >= 1 and T <= 15:
-        if Q:
-            R = '0.0363'
+if PRINCIPAL >= 0 and PRINCIPAL <= 199999:
+    if DURATION >= 1 and DURATION <= 15:
+        if PREQUALIFICATION:
+            RATE = '0.0363'
         else:
-            R = '0.0465'
-    elif T >= 16 and T <= 20:
-        if Q:
-            R = '0.0404'
+            RATE = '0.0465'
+    elif DURATION >= 16 and DURATION <= 20:
+        if PREQUALIFICATION:
+            RATE = '0.0404'
         else:
-            R = '0.0498'
-    elif T >= 21 and T <= 30:
-        if Q:
-            R = '0.0577'
+            RATE = '0.0498'
+    elif DURATION >= 21 and DURATION <= 30:
+        if PREQUALIFICATION:
+            RATE = '0.0577'
         else:
-            R = '0.0639'
-elif P >= 200000 and P <= 999999:
-    if T >= 1 and T <= 15:
-        if Q:
-            R = '0.0302'
+            RATE = '0.0639'
+elif PRINCIPAL >= 200000 and PRINCIPAL <= 999999:
+    if DURATION >= 1 and DURATION <= 15:
+        if PREQUALIFICATION:
+            RATE = '0.0302'
         else:
-            R = '0.0398'
-    elif T >= 16 and T <= 20:
-        if Q:
-            R = '0.0327'
+            RATE = '0.0398'
+    elif DURATION >= 16 and DURATION <= 20:
+        if PREQUALIFICATION:
+            RATE = '0.0327'
         else:
-            R = '0.0408'
-    elif T >= 21 and T <= 30:
-        if Q:
-            R = '0.0466'
-elif P >= 1000000:
-    if T >= 1 and T <= 15:
-        if Q:
-            R = '0.0205'
-    elif T >= 16 and T <= 20:
-        if Q:
-            R = '0.0262'
+            RATE = '0.0408'
+    elif DURATION >= 21 and DURATION <= 30:
+        if PREQUALIFICATION:
+            RATE = '0.0466'
+elif PRINCIPAL >= 1000000:
+    if DURATION >= 1 and DURATION <= 15:
+        if PREQUALIFICATION:
+            RATE = '0.0205'
+    elif DURATION >= 16 and DURATION <= 20:
+        if PREQUALIFICATION:
+            RATE = '0.0262'
 
-if R is not None:
-    R = decimal.Decimal(R)
-    TOTAL = P * ((1 + R/N) ** (N * T))
+if RATE is not None:
+    RATE = decimal.Decimal(R)
+    TOTAL = PRINCIPAL * ((1 + RATE / INTERVAL) ** (INTERVAL * T))
     TOTAL = int(round(TOTAL))
