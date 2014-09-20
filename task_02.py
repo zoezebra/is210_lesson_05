6391d3aba7f6c07617dbded0fd8a6ee4b6beb325
Chad Heuschober <chad.heuschober@cuny.edu>
2014-09-20 12:15:59 -0400

    Corrects quoting.

--- task_02.py
+++ task_02.py
@@ -5,16 +5,19 @@
 import decimal
 
 
-# Your functions should not need or take string representations of these values
+# Your functions should not take string representations of these values
+# Delete these lines when function conversion is complete
 PSTR = raw_input('What is the amount of your principal?: ')
 TSTR = raw_input('For how many years is this loan being borrowed?: ')
 QSTR = raw_input('Are you prequalified for this loan?: ')
 
 # Function arguments should not need the following three transformations
+# Delete these lines when function conversion is complete
 P = int(PSTR)
 T = int(TSTR)
 Q = True if QSTR.lower()[0] == 'y' else False
 
+# Function conversion work you do should start here
 N = 12
 R = None
 TOTAL = None
@@ -22,40 +25,40 @@ TOTAL = None
 if P >= 0 and P <= 199999:
     if T >= 1 and T <= 15:
         if Q:
-            R = "0.0363"
+            R = '0.0363'
         else:
-            R = "0.0465"
+            R = '0.0465'
     elif T >= 16 and T <= 20:
         if Q:
-            R = "0.0404"
+            R = '0.0404'
         else:
-            R = "0.0498"
+            R = '0.0498'
     elif T >= 21 and T <= 30:
         if Q:
-            R = "0.0577"
+            R = '0.0577'
         else:
-            R = "0.0639"
+            R = '0.0639'
 elif P >= 200000 and P <= 999999:
     if T >= 1 and T <= 15:
         if Q:
-            R = "0.0302"
+            R = '0.0302'
         else:
-            R = "0.0398"
+            R = '0.0398'
     elif T >= 16 and T <= 20:
         if Q:
-            R = "0.0327"
+            R = '0.0327'
         else:
-            R = "0.0408"
+            R = '0.0408'
     elif T >= 21 and T <= 30:
         if Q:
-            R = "0.0466"
+            R = '0.0466'
 elif P >= 1000000:
     if T >= 1 and T <= 15:
         if Q:
-            R = "0.0205"
+            R = '0.0205'
     elif T >= 16 and T <= 20:
         if Q:
-            R = "0.0262"
+            R = '0.0262'
 
 if R is not None:
     R = decimal.Decimal(R)
